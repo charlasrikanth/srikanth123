8ab023586a3c7b6e3259347ff732de233dca29ac
charlasrikanth <38878182+charlasrikanth@users.noreply.github.com>
2018-05-22 09:42:37 +0530

    singleline comments removed

--- trailer.py
+++ trailer.py
@@ -5,7 +5,6 @@ print("content-type:text/html \n")
 spiderman1 = media.Movie("spider man-1",
                          "https://bit.ly/2GD3KRc",
                          "https://www.youtube.com/embed/TYMMOjBUPMM")
-# print(spider man-1.storyline)
 spiderman2 = media.Movie("spider man-2",
                          "https://bit.ly/2KCR8Mk",
                          "https://www.youtube.com/embed/BWsLc3j1AWg")
@@ -22,9 +21,7 @@ Spiderman_homecoming_2 = media.Movie("Spider man home coming-2",
                                      "https://bit.ly/2s0z5YO",
                                      "https://www.youtube.com"
                                      "/embed/8QYZQiBVx18")
-# (spider man-1.show_trailer())
 movies = [spiderman1, spiderman2,
           Amazing_spiderman, Spiderman_homecoming, Spiderman_homecoming_2]
 fresh_tomatoes.open_movies_page(movies)
-# print(media.Movie.__doc__)
 
