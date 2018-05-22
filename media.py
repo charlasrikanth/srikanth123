4b0cc7b08216ed2bef1256eb46ba47275b6ea56e
charlasrikanth <38878182+charlasrikanth@users.noreply.github.com>
2018-05-22 09:57:12 +0530

    added attributes

--- media.py
+++ media.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python
 import webbrowser
 print("content-type:text/html \n")
 
 
 class Movie():
-        VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
+                """The attributes of movie trailer are:
+                Attributes:
+                movie_title:Specifies movie title.
+                poster_image:gives movie poster.
+                trailer_youtube:gives youtube link.
+                """
+                VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
 
-        def __init__(self, movie_title, poster_image, trailer_youtube):
-                self.title = movie_title
-                self.poster_image_url = poster_image
-                self.trailer_youtube_url = trailer_youtube
+                def __init__(self, movie_title, poster_image, trailer_youtube):
+                                self.title = movie_title
+                                self.poster_image_url = poster_image
+                                self.trailer_youtube_url = trailer_youtube
 
-        def show_trailer(self):
-                webbrowser.open(self.trailer_youtube_url)
-
+                def show_trailer(self):
+                    webbrowser.open(self.trailer_youtube_url)
